700af5e98836d366e110887e1e8c685fd612e17c
Salva <salvatigua@gmail.com>
2026-06-08 10:30:44 -0300

    guia 1: spring y mysql

---
+++ backend/src/test/java/ar/edu/universidad/gestion/GestionAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.universidad.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
